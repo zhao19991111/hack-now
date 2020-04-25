848bde80c2e5d2bfb91b82c28f3b9752ac9b8719
PabS729 <mishi@ucsd.edu>
2020-04-25 13:22:07 -0700

    db

--- backend/item_add.py
+++ backend/item_add.py
@@ -5,7 +5,7 @@ from selector import *
 def perform_search(item_name, quantity, zip_code='90024', store='walmart'):
     product_list = searchWithIds(item_name, quantity, zip_code, store)
     for product in product_list:
-        add_item(item_name, product['product_id'], product)
+        add_item("item", item_name, product)
 
 
 perform_search('milk', 3)
